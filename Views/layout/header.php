2c9d1eb00f2c251388600b7e49c21dbbff419cfd
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-06-12 11:17:04 +0900

    headerの編集

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Text Snipetter</title>
+    <title>PeChat</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="/css/output.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
@@ -18,11 +18,11 @@
                     Home
                 </a>
             </li>
-            <li id="imageList" value="off" class="me-2 border-b-2 hover:border-gray-400">
+            <!-- <li id="imageList" value="off" class="me-2 border-b-2 hover:border-gray-400">
                 <a href="/post" class="active inline-block p-4 rounded-t-lg text-gray-500 hover:text-black hover:border-gray-300" style="text-decoration:none;">
                     create Post
                 </a>
-            </li>
+            </li> -->
         </ul>
     </div>
 </header>
